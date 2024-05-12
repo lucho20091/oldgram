07c8c88e56d162f42c8fe070fe6625168c1a6f35
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-05-12 15:02:55 -0500

    y

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 const commentSection = document.getElementById('comment-section');
 const sendCommentBtn = document.getElementById('send-comment');
 const inputComment = document.getElementById('input-comment');
+// input and buttons for other posts
+const commentSection2 = document.getElementById('comment-section2');
+const sendCommentBtn2 = document.getElementById('send-comment2');
+const inputComment2 = document.getElementById('input-comment2');
+// input and buttons for other posts
+const commentSection3 = document.getElementById('comment-section3');
+const sendCommentBtn3 = document.getElementById('send-comment3');
+const inputComment3 = document.getElementById('input-comment3');
+// get localstorage item
+const nameFromLocalStorage = JSON.parse(localStorage.getItem('username'))
+// get modal
+const modal = document.getElementById('modal');
+const signInBtn = document.getElementById('sign-in');
+const username = document.getElementById('username');
+
+signInBtn.addEventListener('click', () => {
+    localStorage.setItem('username', username.value)
+})
 
 sendCommentBtn.addEventListener('click', () =>{
     commentSection.innerHTML += `
     <p>${inputComment.value}</p>`
+    localStorage.setItem('username', JSON.stringify(inputComment.value))
     inputComment.value = '';
+    console.log(nameFromLocalStorage); 
 })
+
+sendCommentBtn2.addEventListener('click', () =>{
+    commentSection2.innerHTML += `
+    <p>${inputComment2.value}</p>`
+    inputComment2.value = '';
+})
+
+sendCommentBtn3.addEventListener('click', () =>{
+    commentSection3.innerHTML += `
+    <p>${inputComment3.value}</p>`
+    inputComment3.value = '';
+})
+
+
